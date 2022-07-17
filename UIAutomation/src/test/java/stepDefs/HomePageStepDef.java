7d7b9e4d47e81ec964fce45c967b4fa25b9aff6e
Prince Kumar <prince.kumar_rft>
2022-07-17 20:59:42 +0530

    added logger

--- UIAutomation/src/test/java/stepDefs/HomePageStepDef.java
+++ UIAutomation/src/test/java/stepDefs/HomePageStepDef.java
@@ -2,12 +2,18 @@ package stepDefs;
 
 import driverManager.drivers.CreateDriver;
 import io.cucumber.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import pages.HomePage;
+
+import java.lang.invoke.MethodHandles;
+
 
 public class HomePageStepDef {
     WebDriver driver= CreateDriver.getInstance().getDriver();
     HomePage homePageObjects;
+    Logger logger = LogManager.getLogger(MethodHandles.lookup().getClass().getName());
 
     @Given("user click on {string}")
     public void clickElement(String option){
--- UIAutomation/src/test/java/stepDefs/LoginStepDef.java
+++ UIAutomation/src/test/java/stepDefs/LoginStepDef.java
@@ -3,13 +3,18 @@ package stepDefs;
 import driverManager.drivers.CreateDriver;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.Map;
 
 public class LoginStepDef {
+    Logger logger= LogManager.getLogger(MethodHandles.lookup().getClass().getName());
     WebDriver driver= CreateDriver.getInstance().getDriver();
     LoginPage loginPageObjects;
 
@@ -19,6 +24,7 @@ public class LoginStepDef {
         driver.get(url);
         System.out.println(Thread.currentThread().getId());
         System.out.println(driver.hashCode());
+        logger.info("browser is launched");
     }
 
     @Given("user has logged in to application using following credentials")
@@ -28,6 +34,7 @@ public class LoginStepDef {
         loginPageObjects.userName.sendKeys(credentials.get(0).get("UserName"));
         loginPageObjects.password.sendKeys(credentials.get(0).get("Password"));
         loginPageObjects.loginButton.click();
+        logger.info("User has logged in to app");
 
 
     }
